16549b8edef0bd7ab3e1682cf3212f5b1f887411
adonnangel <antonio.donnangelo@libero.it>
2023-09-22 21:12:50 +0200

    Aggiunti serpente e cibo

--- snake.py
+++ snake.py
@@ -1,11 +1,23 @@
 # importing libraries
-import pygame
-import time
-import random
- 
-snake_speed = 15
- 
-# Window size
-window_x = 720
-window_y = 480
- 
+import arcade 
+
+def main():
+    #Set a window
+    arcade.open_window(700,600,"SNAKE")
+
+    # Set the background color
+    arcade.set_background_color(arcade.csscolor.YELLOW_GREEN)
+
+    # Get ready to draw
+    arcade.start_render()
+
+
+    snake = arcade.draw_rectangle_filled(100, 320, 20, 60, arcade.csscolor.SIENNA)
+    food = arcade.draw_circle_filled(100, 220, 10, arcade.csscolor.RED)
+
+    # Finish drawing
+    arcade.finish_render()
+
+    arcade.run()
+
+main()
